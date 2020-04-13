ded628d1fcf5f76b23b75fd5da28058501bc15af
ziming.zhang <ziming.zhang@yitu-inc.com>
2020-04-13 15:01:32 +0800

    update

---
+++ Java/1.8-ssh/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "USER_ID=$USER_ID USER_NAME=$USER_NAME" >> /tmp/x.log
+echo "GROUP_ID=$GROUP_ID GROUP_NAME=$GROUP_NAME" >> /tmp/x.log
+echo "USER_PASSWORD $USER_PWD" >> /tmp/x.log
+
+addgroup -g $GROUP_ID -S $GROUP_NAME 
+adduser -u $USER_ID -s /bin/ash -S $USER_NAME -G $GROUP_NAME
+
+echo "$USER_NAME:$USER_PWD" | chpasswd
+
+# run ssh
+ssh-keygen -t rsa -P "" -f /etc/ssh/ssh_host_rsa_key
+ssh-keygen -t ecdsa -P "" -f /etc/ssh/ssh_host_ecdsa_key
+ssh-keygen -t ed25519 -P "" -f /etc/ssh/ssh_host_ed25519_key
+/usr/sbin/sshd -D
