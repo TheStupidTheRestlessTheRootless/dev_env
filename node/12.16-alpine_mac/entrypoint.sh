b4abfa38c55677cb7f873ab3dca87b30012274c4
ziming.zhang <ziming.zhang@yitu-inc.com>
2020-04-23 17:24:42 +0800

    add node

---
+++ node/12.16-alpine_mac/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+echo "USER_ID=$USER_ID USER_NAME=$USER_NAME" >> /tmp/x.log
+echo "GROUP_ID=$GROUP_ID GROUP_NAME=$GROUP_NAME" >> /tmp/x.log
+echo "USER_PASSWORD $USER_PWD" >> /tmp/x.log
+
+groupadd -g $GROUP_ID -r $GROUP_NAME 
+useradd -u $USER_ID -s /bin/bash -m $USER_NAME -G $GROUP_NAME
+
+echo "$USER_NAME:$USER_PWD" | chpasswd
+
+echo "[[ -s /etc/bash.bashrc ]] && source /etc/bash.bashrc" > /home/$USER_NAME/.bash_profile
+
+# run ssh
+ssh-keygen -t rsa -P "" -f /etc/ssh/ssh_host_rsa_key
+ssh-keygen -t ecdsa -P "" -f /etc/ssh/ssh_host_ecdsa_key
+ssh-keygen -t ed25519 -P "" -f /etc/ssh/ssh_host_ed25519_key
+/usr/sbin/sshd -D
